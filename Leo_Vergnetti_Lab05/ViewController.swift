f271843e5a600c499ffeccc22febee38543dd7cb
Leo Vergnetti <tuj52136@cst11513.tu.temple.edu>
2019-03-28 11:26:00 -0400

    Initial Commit

---
+++ Leo_Vergnetti_Lab05/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leo_Vergnetti_Lab05
+//
+//  Created by Leo Vergnetti on 3/28/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Leo_Vergnetti_Lab05Tests/Leo_Vergnetti_Lab05Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Leo_Vergnetti_Lab05Tests.swift
+//  Leo_Vergnetti_Lab05Tests
+//
+//  Created by Leo Vergnetti on 3/28/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import XCTest
+@testable import Leo_Vergnetti_Lab05
+
+class Leo_Vergnetti_Lab05Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
